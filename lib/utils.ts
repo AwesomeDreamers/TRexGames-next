17ac1d9c6b6ce225fb770632855454cd72fab601
Gyulo94 <qkrcjfrb112@gmail.com>
2025-04-09 11:53:45 -0300

    Refactor :  원화 변경 로직

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function priceFormat(price: number) {
+export function originalPrice(price: number) {
   const formattedPrice = price.toLocaleString("ko-KR", {
     style: "currency",
     currency: "KRW",
@@ -13,11 +13,23 @@ export function priceFormat(price: number) {
   return formattedPrice;
 }
 
-export function calcDiscountedPrice(
+export function currencyPrice(
   originalPrice: number,
   discountPercentage: number
 ) {
+  if (discountPercentage <= 0) {
+    const formattedPrice = originalPrice.toLocaleString("ko-KR", {
+      style: "currency",
+      currency: "KRW",
+    });
+    return formattedPrice;
+  }
   const discountAmount = (originalPrice * discountPercentage) / 100;
   const discountedPrice = originalPrice - discountAmount;
-  return discountedPrice;
+
+  const formattedPrice = discountedPrice.toLocaleString("ko-KR", {
+    style: "currency",
+    currency: "KRW",
+  });
+  return formattedPrice;
 }
