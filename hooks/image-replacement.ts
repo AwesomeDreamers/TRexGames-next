9126c70595b58d16041a109fddbb4d15e017c413
Gyulo94 <qkrcjfrb112@gmail.com>
2025-04-20 23:37:02 -0300

    feat: react quilljs 이미지 업로드시 url 경로 변경하는 훅 추가

---
+++ hooks/image-replacement.ts
@@ -0,0 +1,36 @@
+"use client";
+import { changePathContentImage } from "@/actions/file.actions";
+import { useState } from "react";
+
+export default function useQuillImageReplacement() {
+  const [urlArray, setUrlArray] = useState<string[]>([]);
+
+  const replaceImages = async (content: string) => {
+    const srcArray: string[] = [];
+    const gainSource = /<img[^>]*src\s*=\s*["']?([^>"']+)["']?[^>]*>/g;
+    let endContent = content;
+
+    let match;
+    while ((match = gainSource.exec(content)) !== null) {
+      const result = match[1];
+      console.log("src 추출 결과:", result);
+      srcArray.push(result);
+    }
+
+    const requestData = { urls: srcArray };
+
+    try {
+      const urls = await changePathContentImage(requestData);
+      setUrlArray(urls);
+      srcArray.forEach((src, index) => {
+        endContent = endContent.replace(src, urls[index]);
+      });
+    } catch (error) {
+      console.error("이미지 업로드 실패:", error);
+    }
+
+    return endContent;
+  };
+
+  return { urlArray, replaceImages };
+}
