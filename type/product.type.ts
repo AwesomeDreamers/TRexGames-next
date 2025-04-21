6a2b346696f709ac033a120cdbd10c03f454d4e7
Gyulo94 <qkrcjfrb112@gmail.com>
2025-04-21 04:19:06 -0300

    chore: Product 타입 추가

--- type/product.type.ts
+++ type/product.type.ts
@@ -2,3 +2,17 @@ import { ProductFormSchema } from "@/validation/product.schema";
 import { z } from "zod";
 
 export type ProductFormType = z.infer<typeof ProductFormSchema>;
+export type ProductType = z.infer<typeof ProductFormSchema> & {
+  id: number;
+  category: {
+    id: number;
+    name: string;
+  };
+  platform: {
+    id: number;
+    name: string;
+  };
+  rating: number;
+  createdAt: string;
+  updatedAt: string;
+};
