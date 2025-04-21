63fab8874037e62c6af568d060e99419e846ce93
Gyulo94 <qkrcjfrb112@gmail.com>
2025-04-21 04:16:52 -0300

    feat: 원화 가격 포맷함수 추가

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,27 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function currencyPrice(
+  originalPrice: number,
+  discountPercentage: number
+) {
+  if (discountPercentage <= 0) {
+    const formattedPrice = originalPrice.toLocaleString("ko-KR", {
+      style: "currency",
+      currency: "KRW",
+    });
+    return formattedPrice;
+  }
+  const discountAmount = (originalPrice * discountPercentage) / 100;
+  const discountedPrice = originalPrice - discountAmount;
+
+  const formattedPrice = discountedPrice.toLocaleString("ko-KR", {
+    style: "currency",
+    currency: "KRW",
+  });
+  return formattedPrice;
 }
