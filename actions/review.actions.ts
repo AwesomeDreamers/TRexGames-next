0653e0e911d836ff07a7ed8cff17d5e4385eaca2
Gyulo94 <qkrcjfrb112@gmail.com>
2025-05-06 17:10:52 -0300

    feat: 리뷰 api 통신 액션 추가

---
+++ actions/review.actions.ts
@@ -0,0 +1,88 @@
+"use server";
+import { SERVER_URL } from "@/constants/common";
+import { ReviewFilterType, ReviewFormType } from "@/type/review.type";
+import axios from "axios";
+import { auth } from "../auth";
+
+export async function createReview(values: ReviewFormType) {
+  const session = await auth();
+  const token = session?.serverTokens.access_token;
+  const response = await axios.post(`${SERVER_URL}/review/create`, values, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const { status, message, payload } = response.data;
+  return { status, message, payload };
+}
+
+export async function findReviewsAll() {
+  const response = await axios.get(`${SERVER_URL}/review/all`);
+  const { status, message, payload } = response.data;
+  return { status, message, payload };
+}
+
+export async function findReviewByProductIdAndUserId(productId?: number) {
+  const session = await auth();
+  const token = session?.serverTokens.access_token;
+  const response = await axios.get(
+    `${SERVER_URL}/review/user-id/product-id/${productId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const { status, message, payload } = response.data;
+  return { status, message, payload };
+}
+
+export async function findReviewsByProductId(filters: ReviewFilterType) {
+  const {
+    currentPage = 1,
+    sortBy = "createdAt",
+    sortOrder = "desc",
+    productId,
+  } = filters;
+  const page = currentPage;
+  const params = new URLSearchParams();
+  if (page) params.append("page", page.toString());
+  if (sortBy !== "createdAt" || sortOrder !== "desc") {
+    params.append("sortOrder", sortOrder);
+    params.append("sortBy", sortBy);
+  }
+  const response = await axios.get(
+    `${SERVER_URL}/review/product-id/${productId}?${params.toString()}`
+  );
+  const { status, message, payload } = response.data;
+  return { status, message, payload };
+}
+
+export async function updateReview(values: ReviewFormType, id?: string) {
+  const session = await auth();
+  const token = session?.serverTokens.access_token;
+  const response = await axios.put(
+    `${SERVER_URL}/review/update/${id}`,
+    values,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const { status, message, payload } = response.data;
+  return { status, message, payload };
+}
+export async function deleteReview(productId?: number) {
+  const session = await auth();
+  const token = session?.serverTokens.access_token;
+  const response = await axios.delete(
+    `${SERVER_URL}/product/delete/${productId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  return response.data;
+}
