50144deeeb4aeb63c5904a459978898ea9365b60
Gyulo94 <qkrcjfrb112@gmail.com>
2025-05-06 16:46:18 -0300

    design: 상품 등록  에디터 높이값 변경

--- components/admin/products/add/react-quill-editor.tsx
+++ components/admin/products/add/react-quill-editor.tsx
@@ -3,10 +3,12 @@ import BlotFormatter from "quill-blot-formatter";
 import "quill/dist/quill.snow.css";
 import { useEffect } from "react";
 import { useQuill } from "react-quilljs";
-
+import { toast } from "sonner";
 const ReactQuillEditor = ({ ...props }: any) => {
   const { quill, quillRef, Quill } = useQuill({
-    modules: { blotFormatter: {} },
+    modules: {
+      blotFormatter: {},
+    },
   });
 
   if (Quill && !quill) {
@@ -32,7 +34,7 @@ const ReactQuillEditor = ({ ...props }: any) => {
         input.onchange = async () => {
           const file = input.files?.[0];
           if (!file) {
-            console.error("파일이 선택되지 않았습니다.");
+            toast.error("파일이 선택되지 않았습니다.");
             return;
           }
 
@@ -48,7 +50,7 @@ const ReactQuillEditor = ({ ...props }: any) => {
               quill.setSelection(range.index + 1);
             }
           } catch (error) {
-            console.error("이미지 업로드 중 오류 발생:", error);
+            toast.error("이미지 업로드 중 오류가 발생하였습니다.");
           }
         };
       });
@@ -56,8 +58,8 @@ const ReactQuillEditor = ({ ...props }: any) => {
   }, [quill, Quill]);
 
   return (
-    <div>
-      <div ref={quillRef} />
+    <div className="mb-11">
+      <div ref={quillRef} className="min-h-[180px] max-h-[800px]" />
     </div>
   );
 };
